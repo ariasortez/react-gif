130dddbc9d177dafad10688360565ee8b82298b1
ariasortez <juan.arias@unitec.edu>
2023-02-19 18:44:32 -0600

    Testing

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -15,6 +15,7 @@ export const GifExpertApp = () => {
 
             {categories.map((category) => (
                 <GifGrid key={category} category={category} />
+                sa
             ))}
         </>
     );
